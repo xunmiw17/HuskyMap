e0a702e439e6446f6f75cf3844d084b44c4ade55
kate2001 <kate2001@cs.washington.edu>
2022-05-26 18:07:06 -0700

    Staff push of files

---
+++ hw-campuspaths/src/App.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 Kevin Zatloukal.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Spring Quarter 2022 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+import React, {Component} from 'react';
+
+// Allows us to write CSS styles inside App.css, any styles will apply to all components inside <App />
+import "./App.css";
+
+class App extends Component<{}, {}> {
+
+    render() {
+        return (
+            <p>Here's the beginning of your AMAZING CampusPaths GUI!</p>
+        );
+    }
+
+}
+
+export default App;
